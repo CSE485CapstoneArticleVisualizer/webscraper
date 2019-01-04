312796696381ee0ad3da6fe7e7955e55ebe9939d
Alex <alexnou@gmail.com>
2019-01-04 00:53:27 -0600

    Allowed NORMAL tier messages to appear in logs

--- WebScraperLogger.py
+++ WebScraperLogger.py
@@ -34,7 +34,7 @@ class WebScraperLogger():
   def log(self, filename, message, priority=Priority.NORMAL):
     # Save to_be_visited_pages
     save_to_file = True
-    if save_to_file and priority.value > Priority.NORMAL.value and priority.value != Priority.ARTICLE_DETAILS.value:
+    if save_to_file and priority.value > Priority.LOW.value and priority.value != Priority.ARTICLE_DETAILS.value:
       target = './{}'.format(filename)
       with open(target, 'a') as outfile:
         outfile.write("%s %s\n" % (datetime.datetime.now().strftime("[%I:%M%p on %B %d, %Y]"), message))
