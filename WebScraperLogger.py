f83c12a5f4617ae0111cea080d26b11819994b10
Alex <alexnou@gmail.com>
2019-01-04 00:31:05 -0600

    Disabled logging to file

--- WebScraperLogger.py
+++ WebScraperLogger.py
@@ -33,8 +33,8 @@ class WebScraperLogger():
     print("------------------------------------------------------------------")
   def log(self, filename, message, priority=Priority.NORMAL):
     # Save to_be_visited_pages
-    
-    if priority.value > Priority.LOW.value and priority.value != Priority.ARTICLE_DETAILS.value:
+    save_to_file = False
+    if save_to_file and priority.value > Priority.NORMAL.value and priority.value != Priority.ARTICLE_DETAILS.value:
       target = './{}'.format(filename)
       with open(target, 'a') as outfile:
         outfile.write("%s %s\n" % (datetime.datetime.now().strftime("[%I:%M%p on %B %d, %Y]"), message))
