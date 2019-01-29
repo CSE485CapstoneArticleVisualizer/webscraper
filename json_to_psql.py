aebbde78f3126323d1e9ed703fdd00fe3c76ba0b
Alex <alexnou@gmail.com>
2019-01-28 17:26:16 -0700

    Fix

--- json_to_psql.py
+++ json_to_psql.py
@@ -4,22 +4,23 @@ import json
 import psycopg2
 
 conn = psycopg2.connect("host=localhost dbname=stephen user=stephen password=stephen")
-cur = connection.cursor()
+cur = conn.cursor()
 
 with open('./Data/2019-01-28/2019-03-01--Demystifying the impact of CEO transformational leadership on firm performance Interactive roles of exploratory innovation and environmental uncertainty.json') as json_data:
 #with open('./Data/2019-01-28/2017-09-01--YouTube as a Source of Information on Neurosurgery.json') as json_data:    
     
     # data here is a list of dicts
     data = [json.load(json_data)]
-    print(data)
+    
 
     # create a table with one column of type JSON
-    # cur.execute("CREATE TABLE t_skaters (data json);")
+    cur.execute("CREATE TABLE SMA (data json);")
 
     fields = ['abstract', 'authors', 'citationCount', 'citedBy', 'citedByCount', 'cites', 'citesCount', 'date', 'journal', 'referenceCount', 'title'];
 
     for item in data:
         my_data = {field: item[field] for field in fields}
+        print("FINAL DATA\n", my_data)
         cur.execute("INSERT INTO articles VALUES (%s)", (json.dumps(my_data),))
 
 
@@ -27,5 +28,6 @@ with open('./Data/2019-01-28/2019-03-01--Demystifying the impact of CEO transfor
     conn.commit()
     # Close the connection
     conn.close()
+    print("Success!")
 
     # -------------------------------------
