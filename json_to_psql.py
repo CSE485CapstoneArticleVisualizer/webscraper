c89dddf02cd69d572a906b1e38cd34ccaa9f70b3
Alex <alexnou@gmail.com>
2019-01-28 17:20:43 -0700

    Fixed json_to_psql.py file?

--- json_to_psql.py
+++ json_to_psql.py
@@ -3,29 +3,29 @@
 import json
 import psycopg2
 
-connection = psycopg2.connect("host=localhost dbname=stephen user=stephen password=stephen")
-cursor = connection.cursor()
+conn = psycopg2.connect("host=localhost dbname=stephen user=stephen password=stephen")
+cur = connection.cursor()
 
 with open('./Data/2019-01-28/2019-03-01--Demystifying the impact of CEO transformational leadership on firm performance Interactive roles of exploratory innovation and environmental uncertainty.json') as json_data:
 #with open('./Data/2019-01-28/2017-09-01--YouTube as a Source of Information on Neurosurgery.json') as json_data:    
     
-    data = json.load(json_data)
+    # data here is a list of dicts
+    data = [json.load(json_data)]
     print(data)
-    # Populate column fields
+
+    # create a table with one column of type JSON
+    # cur.execute("CREATE TABLE t_skaters (data json);")
+
     fields = ['abstract', 'authors', 'citationCount', 'citedBy', 'citedByCount', 'cites', 'citesCount', 'date', 'journal', 'referenceCount', 'title'];
 
-    # Store JSON in databse
-    # for item in data:
-    #     my_data = [item[field] for field in fields]
-    #     for i, v in enumerate(my_data):
-    #         if isinstance(v, dict):
-    #             my_data[i] = json.dumps(v)
-    #     insert_query = "INSERT INTO articles VALUES " 
-    #     cursor.execute(insert_query, tuple(my_data))
+    for item in data:
+        my_data = {field: item[field] for field in fields}
+        cur.execute("INSERT INTO articles VALUES (%s)", (json.dumps(my_data),))
 
-    my_data = [data[field] for field in fields]
-    for i, v in enumerate(my_data):
-        if isinstance(v, dict):
-            my_data[i] = json.dumps(v)
 
-    cursor.execute("INSERT INTO articles (info) VALUES (%s)", str(data))
+    # commit changes
+    conn.commit()
+    # Close the connection
+    conn.close()
+
+    # -------------------------------------
