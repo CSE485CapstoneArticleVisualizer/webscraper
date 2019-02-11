fe897dc98eebc7ddb8c29046334ca45ffa106a4e
Nicholas Jorgensen <nicholas.k.jorgensen@gmail.com>
2019-02-11 14:12:44 -0700

    Added Query: Get Articles In Journal  +1
    Fixed Query: Get Articles For Author

--- postgres_queries/get_articles_for_author.py
+++ postgres_queries/get_articles_for_author.py
@@ -3,8 +3,8 @@
 import psycopg2
 
 def getArticlesForAuthor(author) :
-    cur.execute("SELECT S.* FROM sma S, article_authors A WHERE S.id = A.article_id AND A.article_id = " + author)
-    print(cur.fetchAll())
+    cur.execute("SELECT S.* FROM sma S, article_authors A WHERE S.id = A.article_id AND A.author_name = %s", (author, ))
+    print(cur.fetchall())
 
 if __name__ == "__main__":
     conn = psycopg2.connect("host=localhost port=5434 dbname=stephen user=stephen password=stephen")
--- postgres_queries/get_articles_in_journal.py
+++ postgres_queries/get_articles_in_journal.py
@@ -1,1 +1,13 @@
 # TODO: Get the articles that are stored in the database for a given journal
+
+import psycopg2
+
+def getArticlesForJournal(journal) :
+    cur.execute("SELECT S.* FROM sma S, journals J WHERE S.journal_id = J.id AND J.name = %s", (journal, ))
+    print(cur.fetchall())
+
+if __name__ == "__main__":
+    conn = psycopg2.connect("host=localhost port=5434 dbname=stephen user=stephen password=stephen")
+    cur = conn.cursor()
+
+    getArticlesForJournal("Research Policy")
