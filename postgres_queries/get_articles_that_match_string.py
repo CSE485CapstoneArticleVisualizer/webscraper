996ea7eafe070ea81264fef32b556d80ca280530
Nicholas Jorgensen <nicholas.k.jorgensen@gmail.com>
2019-02-11 14:27:00 -0700

    Added Query: Get Articles That Match String

--- postgres_queries/get_articles_that_match_string.py
+++ postgres_queries/get_articles_that_match_string.py
@@ -1,1 +1,13 @@
 # TODO: Get the articles whose title matches a certain string
+
+import psycopg2
+
+def getArticlesThatMatchString(string) :
+    cur.execute("SELECT S.* FROM sma S WHERE S.title = %s", (string, ))
+    print(cur.fetchall())
+
+if __name__ == "__main__":
+    conn = psycopg2.connect("host=localhost port=5434 dbname=stephen user=stephen password=stephen")
+    cur = conn.cursor()
+
+    getArticlesThatMatchString("Linkages and Spillovers")
