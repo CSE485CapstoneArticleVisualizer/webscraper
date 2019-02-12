6e58ebb4ac3b70230da5d00ecb48e16abb6f8113
Nicholas Jorgensen <nicholas.k.jorgensen@gmail.com>
2019-02-12 11:13:26 -0700

    Added Query: Get Cited By Articles

--- postgres_queries/get_cited_by_articles.py
+++ postgres_queries/get_cited_by_articles.py
@@ -1,1 +1,14 @@
-# TODO: Get the articles that the priamry article is cited by.
+# TODO: Get the articles that the 
+# primary article is cited by.
+
+import psycopg2
+
+def getCitedByArticles(articleID) :
+    cur.execute("SELECT S.* FROM sma S, (SELECT C.* FROM cited_by C WHERE C.article_id = %s) C WHERE S.id = C.cited_by_id", (articleID, ))
+    print(cur.fetchall())
+
+if __name__ == "__main__":
+    conn = psycopg2.connect("host=localhost port=5434 dbname=stephen user=stephen password=stephen")
+    cur = conn.cursor()
+
+    getCitedByArticles(1)
