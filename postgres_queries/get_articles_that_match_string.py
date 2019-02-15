3bcaedd377d030d8951d4c500f481f91c3819b2e
Alex <alexnou@gmail.com>
2019-02-14 18:30:13 -0700

    Modified get_articles_that_match_string.py

--- postgres_queries/get_articles_that_match_string.py
+++ postgres_queries/get_articles_that_match_string.py
@@ -5,11 +5,11 @@ import psycopg2
 def getArticlesThatMatchString(string) :
     cur.execute("""SELECT S.* 
                  FROM sma S 
-                 WHERE S.title = %s""", (string, ))
+                 WHERE S.title LIKE %s""", (string, ))
     print(cur.fetchall())
 
 if __name__ == "__main__":
     conn = psycopg2.connect("host=localhost port=5434 dbname=stephen user=stephen password=stephen")
     cur = conn.cursor()
 
-    getArticlesThatMatchString("Linkages and Spillovers")
+    getArticlesThatMatchString("%%Phytoextraction of toxic%%")
